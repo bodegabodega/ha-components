a335bd5e1ce463b953a0a05c14d2d3ceaee5e84b
Nikolai Pohorelsky <nikolai.pohorelsky@showtime.net>
2023-11-28 16:26:20 -0500

    1. Added team tracker sample to stub hass object 2. Created team tracker matchup transformer 3. Mapped in matchup domain object to template

--- src/components/team-tracker-element.js
+++ src/components/team-tracker-element.js
@@ -1,4 +1,5 @@
 import { html, css, nothing} from 'lit';
+import { styleMap } from 'lit-html/directives/style-map.js';
 import { BaseComponent } from './base-component';
 import {forEntitiesFromState} from './../lib/matchup';
 import sample from './../../data/hass.json';
@@ -21,14 +22,17 @@ export class TeamTrackerElement extends BaseComponent {
   set hass(h) {
     if (this._config && this._config.entities) {
       this.log('Getting Matchup for Entities State');
-      this.matchup = forEntitiesFromState(this._config.entity, h);
+      this.matchup = forEntitiesFromState(this._config.entities, h);
     }
   }
   setConfig(config) {
     this._config = Object.assign(TeamTrackerElement.getDefaults(), config);
     this.log('Setting Config', this._config)
     if (!config.entities) throw new Error("You need to define entities");
-    if (this._config.mode == 'development') this.hass = sample;
+    if (this._config.mode == 'development') {
+      this._config.entities = ["sensor.arsenal_cl_team_tracker", "sensor.arsenal_team_tracker"];
+      this.hass = sample;
+    }
   }
 
   render() {
@@ -37,25 +41,29 @@ export class TeamTrackerElement extends BaseComponent {
       ? html`
       <div class="outer">
         <div class="row">
-          <div class="detail">Dec 2 &bull; 3:00PM</div>
-          <div class="detail">EPL</div>
+          <div class="detail">${this.matchup.date}</div>
+          <div class="detail">${this.matchup.league}</div>
         </div>
         <div class="row opponents">
-          <div class="home">
-            <div>ARS</div>
-            <div class="detail">9 &bull; 3 &bull; 1</div>
+          <div class="home" style=${styleMap({
+              borderColor: `${this.matchup.home.color}`
+            })}>
+            <div>${this.matchup.home.abbreviation}</div>
+            <div class="detail">${this.matchup.home.record}</div>
           </div>
           <div class="score">
-            <div>2 &bull; 1</div>
-            <div class="detail">FT</div>
+            <div>${this.matchup.score}</div>
+            <div class="detail">${this.matchup.clock}</div>
           </div>
-          <div class="away">
-            <div>WOL</div>
-            <div class="detail">4 &bull; 3 &bull; 5</div>
+          <div class="away" style=${styleMap({
+              borderColor: `${this.matchup.away.color}`
+            })}>
+            <div>${this.matchup.away.abbreviation}</div>
+            <div class="detail">${this.matchup.away.record}</div>
           </div>
         </div>
         <div class="details">
-          <div class="detail">Emirates Stadium &bull; London, England</div>
+          <div class="detail">${this.matchup.location}</div>
         </div>
       </div>
       `
@@ -70,6 +78,8 @@ export class TeamTrackerElement extends BaseComponent {
         display: flex;
         flex-direction: column;
         justify-content: center;
+
+        background-color: var(--color-bg-secondary);
       }
       .outer {
         margin: 10px;
